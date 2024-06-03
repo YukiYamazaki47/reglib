eaab158d206d9237047d6bb32ed2724a2dd4c25b
Yuki Yamazaki <wialwachter@gmail.com>
2024-06-03 22:17:40 +0200

    add

--- include/rio.h
+++ include/rio.h
@@ -3,13 +3,17 @@
 #ifndef LIB_REGLIB
 #define LIB_REGLIB
 
+void default_fn(byte data[]);
+
 class rio {
 public:
     rio();
     void init();
     void update();
+    void set_update();
 private:
     byte buffer[4];
+    void (*fnlut[16])(byte data[2]) = {default_fn};
 };
 
 #endif // LIB_REGLIB
